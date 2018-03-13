ac9f3e8963a99193c231dea4b538292693f8d3d9
DimGael <gael.dim@etu.unilim.fr>
2018-03-13 11:43:15 +0100

    Ajout de la table jeu dans la bdd

---
+++ app/src/main/java/com/android/unilim/ratiog4/ratiog4/sqlite/jeu/Jeu.java
@@ -0,0 +1,34 @@
+package com.android.unilim.ratiog4.ratiog4.sqlite.jeu;
+
+//Pour l'instant on ne gère pas l'image du jeu
+public class Jeu {
+
+    private long id;
+    private String nom;
+    //Image
+
+
+    public Jeu(long id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public Jeu() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
---
+++ app/src/main/java/com/android/unilim/ratiog4/ratiog4/sqlite/jeu/JeuDataSource.java
@@ -0,0 +1,77 @@
+package com.android.unilim.ratiog4.ratiog4.sqlite.jeu;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class JeuDataSource {
+
+    private JeuxSQLiteOpenHelper helper;
+    private SQLiteDatabase database;
+    private final String[] allColumns;
+
+
+    public JeuDataSource(Context context) {
+        this.allColumns = new String[]{
+                JeuxSQLiteOpenHelper.COLONNE_ID,
+                JeuxSQLiteOpenHelper.COLONNE_NOM
+        };
+
+        helper = new JeuxSQLiteOpenHelper(context);
+    }
+
+    public void open() throws SQLException{
+        database = this.helper.getWritableDatabase();
+    }
+
+    public void close(){
+        this.helper.close();
+    }
+
+    public Collection<Jeu> getAllJeux(){
+        Cursor cursor = this.database.query(JeuxSQLiteOpenHelper.TABLE_JEU, allColumns, null, null, null, null, null);
+
+        Collection<Jeu> listeJeux = new ArrayList<Jeu>();
+        while (cursor.moveToNext()) {
+            listeJeux.add(creerJeu(cursor));
+        }
+        cursor.close();
+
+        return listeJeux;
+    }
+
+    public Jeu ajouterJeu(Jeu jeu){
+        ContentValues values = new ContentValues();
+        values.put(JeuxSQLiteOpenHelper.COLONNE_NOM, jeu.getNom());
+
+        long id = this.database.insert(JeuxSQLiteOpenHelper.TABLE_JEU, JeuxSQLiteOpenHelper.COLONNE_NOM, values);
+
+        Cursor cursor = this.database.query(JeuxSQLiteOpenHelper.TABLE_JEU, allColumns, JeuxSQLiteOpenHelper.COLONNE_ID +" = "+id, null, null, null, null);
+
+        Jeu jeuAjoute = new Jeu();
+
+        if(cursor.moveToFirst()){
+            jeuAjoute = creerJeu(cursor);
+        }
+
+        return jeuAjoute;
+    }
+
+    public void supprimerJeu(Jeu jeu){
+        this.supprimerJeu(jeu.getId());
+    }
+
+    public void supprimerJeu(long id) {
+        this.database.delete(JeuxSQLiteOpenHelper.TABLE_JEU, JeuxSQLiteOpenHelper.COLONNE_ID +" = "+id, null);
+    }
+
+    private Jeu creerJeu(Cursor cursor) {
+       return new Jeu(cursor.getInt(JeuxSQLiteOpenHelper.NUM_COLONNE_ID),
+                cursor.getString(JeuxSQLiteOpenHelper.NUM_COLONNE_NOM));
+    }
+}
