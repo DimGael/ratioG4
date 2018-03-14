577a46bb813511b6ad0f58b0df8cc1bfc496b1f2
DimGael <gael.dim@etu.unilim.fr>
2018-03-14 16:58:05 +0100

    Ajout de l'activité ajouter Jeu

---
+++ app/src/main/java/com/android/unilim/ratiog4/ratiog4/AjouterJeuActivity.java
@@ -0,0 +1,17 @@
+package com.android.unilim.ratiog4.ratiog4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dimga on 14/03/2018.
+ */
+
+public class AjouterJeuActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_ajouter_jeu);
+    }
+}
