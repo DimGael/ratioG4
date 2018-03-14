b135c71424dea01b511198d73a1fcfd7fe6bd3bf
DimGael <gael.dim@etu.unilim.fr>
2018-03-14 17:26:58 +0100

    Bouton Ajouter jeu

--- app/src/main/java/com/android/unilim/ratiog4/ratiog4/MainActivity.java
+++ app/src/main/java/com/android/unilim/ratiog4/ratiog4/MainActivity.java
@@ -1,7 +1,10 @@
 package com.android.unilim.ratiog4.ratiog4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.android.unilim.ratiog4.ratiog4.sqlite.JeuAdapter;
@@ -14,7 +17,7 @@ import java.util.List;
  * Created by dimga on 14/03/2018.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private JeuDataSource jeuDataSource;
 
@@ -28,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
         List<Jeu> jeux = this.jeuDataSource.getAllJeux();
 
-
         final ListView viewJeux = (ListView)findViewById(R.id.listViewJeux);
         viewJeux.setAdapter(new JeuAdapter(this, jeux));
+
+        final Button bouton_ajouter_jeu = (Button)findViewById(R.id.bouton_ajouter_jeu);
+        bouton_ajouter_jeu.setOnClickListener(this);
+
     }
 
     @Override
@@ -43,5 +49,16 @@ public class MainActivity extends AppCompatActivity {
     public void onResume(){
         super.onResume();
         this.jeuDataSource.open();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bouton_ajouter_jeu :
+                final Intent intentAjouterJeu = new Intent(MainActivity.this, AjouterJeuActivity.class);
+                this.startActivity(intentAjouterJeu);
+                this.finish();
+                break;
+        }
     }
 }
