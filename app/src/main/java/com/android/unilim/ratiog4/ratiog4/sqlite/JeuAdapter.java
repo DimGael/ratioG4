a08414786b402a75274f06f75a99c2b48b5b457b
maurel11 <guillaume.maurel@etu.unilim.fr>
2018-03-14 17:10:25 +0100

    suppr jeu

--- app/src/main/java/com/android/unilim/ratiog4/ratiog4/sqlite/JeuAdapter.java
+++ app/src/main/java/com/android/unilim/ratiog4/ratiog4/sqlite/JeuAdapter.java
@@ -66,7 +66,6 @@ public class JeuAdapter extends BaseAdapter {
                 jeuDataSource.supprimerJeu(jeu);
                 Intent intent = new Intent(context, MainActivity.class);
                 context.startActivity(intent);
-                context.
                 jeuDataSource.close();
 
             }
