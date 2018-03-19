3a7802f90e36c141fc83755b7189392e4762d2b2
maurel11 <guillaume.maurel@etu.unilim.fr>
2018-03-19 08:34:48 +0100

    modification activité pour rentrer suppr

--- app/src/main/java/com/android/unilim/ratiog4/ratiog4/MainActivity.java
+++ app/src/main/java/com/android/unilim/ratiog4/ratiog4/MainActivity.java
@@ -27,7 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         jeuDataSource = new JeuDataSource(this);
         jeuDataSource.open();
-
+        jeuDataSource.ajouterJeu(new Jeu(1,"coucou"));
+        jeuDataSource.ajouterJeu(new Jeu(2,"un jeu un peu long"));
         List<Jeu> jeux = this.jeuDataSource.getAllJeux();
 
         final ListView viewJeux = (ListView)findViewById(R.id.listViewJeux);
